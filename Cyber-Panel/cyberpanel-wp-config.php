ab81f37a43602000324bd639962419993b46b8b5
Roldan004 <75007596+Roldan004@users.noreply.github.com>
2021-02-17 19:10:13 +0800

    Create cyberpanel-wp-config.php

---
+++ Cyber-Panel/cyberpanel-wp-config.php
@@ -0,0 +1,21 @@
+// test
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'mcgiclassifiedadsdb' );
+
+/** MySQL database username */
+//define( 'DB_USER', 'bindapp' );
+define( 'DB_USER', 'devmcads');
+
+/** MySQL database password */
+//define( 'DB_PASSWORD', 'bindapp123!@#' );
+define( 'DB_PASSWORD', 'v6dpKvD7HBJ745jg' );
+
+/** MySQL hostname */
+define( 'DB_HOST', '172.104.52.5' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
