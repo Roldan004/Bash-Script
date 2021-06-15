c5a9de25bcb97e4660b222a1669cbf4a335d629b
Roldan004 <75007596+Roldan004@users.noreply.github.com>
2021-06-15 13:52:17 +0800

    Update WP-Lemp-Setup.sh

--- LEMP-Wordpress-Setup/WP-Lemp-Setup.sh
+++ LEMP-Wordpress-Setup/WP-Lemp-Setup.sh
@@ -18,15 +18,16 @@ sudo apt install mysql-server mysql-client -y
 sudo apt install php7.2 -y
 
 
-# remove apache content
-sudo apt-get purge apache2 -y
-sudo apt-get autoremove -y
-
 # install some good tools
 sudo apt install git unzip curl -y
 
 # install php modules
 sudo apt install php7.2-bcmath php7.2-curl php7.2-dev php7.2-fpm php7.2-gd php7.2-intl php7.2-mbstring php7.2-xml php7.2-zip php7.2-mysql php-cli php7.2-dev php7.2-fpm -y
+
+
+# remove apache content
+sudo apt-get purge apache2 -y
+sudo apt-get autoremove -y
 
 
 # composer
